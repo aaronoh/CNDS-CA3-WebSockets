01cae86caad431a97f7d6cad0a0c13108d4a75f2
Aaron O'Hare <aaronoh96@gmail.com>
2017-04-02 17:21:20 +0100

    Added styling, client list, desktop notifications, message timestamp

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 // server.js
-var express = require('express');  
-var app = express();  
-var server = require('http').createServer(app); 
-var io = require('socket.io')(server); 
-
-app.use(express.static(__dirname + '/public')); 
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+app.use(express.static(__dirname + '/public'));
 //redirect / to our index.html file
-
-app.get('/', function(req, res,next) {  
+app.get('/', function (req, res, next) {
     res.sendFile(__dirname + '/public/index.html');
 });
-
-io.on('connection', function(client) {  
-	console.log('client connected');
-    client.on('chat message', function(msg){
-        io.emit('chat message', msg)
+io.on('connection', function (client) {
+    console.log('client connected');
+    //when a new client connects
+    io.clients(function (error, clients) {
+        if (error) throw error;
+        //send the list of clients out to all the clients
+        io.emit('clientList', clients);
+    });
+    client.on('chat message', function (message) {
+        io.emit('chat message', message)
     })
-	
 });
-
-
 //start our web server and socket.io server listening
-server.listen(3000, function(){
-  console.log('listening on *:3000');
-}); 
+server.listen(3000, function () {
+    console.log('listening on *:3000');
+});
